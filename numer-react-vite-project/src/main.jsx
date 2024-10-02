b269860d1de408db0fa5692c7ad835efdd60cccc
EnterpriseA1 <than.siriphiwat@gmail.com>
2024-10-02 21:39:51 +0700

    Update main.jsx

--- numer-react-vite-project/src/main.jsx
+++ numer-react-vite-project/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 //import App from './pages/Bisection.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
