99671974cde41dd2b21cc2f4d1fe694b4ea1529f
EnterpriseA1 <than.siriphiwat@gmail.com>
2024-11-01 01:56:56 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.listen(5000, () => {
     console.log("Server is running on port 5000");
 })
 
-readdirSync("./routes").map((r)=>app.use("/api", require("./routes/" + r)));
+//readdirSync("./routes").map((r)=>app.use("/api", require("./routes/" + r)));
 
 
 
