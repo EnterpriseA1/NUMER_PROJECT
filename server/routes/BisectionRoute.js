2ab804d2d45123f6874215d32023be17228306ed
EnterpriseA1 <than.siriphiwat@gmail.com>
2024-11-01 02:45:28 +0700

    Update BisectionRoute.js

--- server/routes/BisectionRoute.js
+++ server/routes/BisectionRoute.js
@@ -4,18 +4,20 @@ const router = express.Router();
 const {read, list, create } = require('../controller/BisectionController');
 
 router.get('/', list);
-router.get('/:id', read);
+
+//router.get('/:id', read);
 
 router.post('/',create);
 
+/*router.get('/',async (req, res) => {
+    try {
+        res.json("hello");
+    } catch (error) {
+        console.error('List error:', error);
+        res.status(500).json({ error: 'Server Error' });
+    }
+});*/
 
-/*router.put('/bisection', (req, res) => {
-    res.send('Hello World! server is running put');
-})
-router.delete('/bisection', (req, res) => {
-    res.send('Hello World! server is running delete');
-    res.json()
-})*/
 
 
 module.exports = router;
